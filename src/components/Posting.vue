e6ecbcac5b3f6773a649a3542592f498c0ed25a4
婆娑 <1461710521@qq.com>
2017-09-15 19:37:53 +0800

    xg

--- src/components/Posting.vue
+++ src/components/Posting.vue
@@ -2,12 +2,14 @@
    <div class="posting">
         <mt-header title="发微博" style="color:black">
             <router-link to="/hello" slot="left">
-                <mt-button icon="back" style="color:black">取消</mt-button>               
+                <mt-button style="color:black;margin-left:15px;">取消</mt-button>               
             </router-link>
             <router-link to="/hello" slot="right">
-                <mt-button @click="popupVisible=true">发送</mt-button>
+                <mt-button style="" class="send">发送</mt-button>
             </router-link>
         </mt-header>
+
+            <textarea id="read" class="read"></textarea>
 
     </div>
 </template>
@@ -17,7 +19,7 @@ export default {
   name: 'Posting',
   data () {
     return {
-        popupVisible:false
+        
     }
   }
 }
@@ -25,7 +27,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    *{
+        margin:0;
+        padding:0;
+    }
    .mint-header{
        background-color:#eee;
+   }
+   #app{
+       position:relative;
+   }
+   .read{
+       position:absolute;
+       top:40px;
+       bottom:50px;
+       left:0;
+       width:100%;
+       border:0;
+   }
+   .send{
+        margin-right:15px;
    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Hello from '@/components/Hello'
 import Posting from '@/components/Posting'
+import Friend from '@/components/Friend'
+import comment from '@/components/comment'
 
 Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/Hello',
-      name: 'This is Hello',
+      name: 'This is Home',
       component: Hello
     },
     {
       path: '/Posting',
       name: 'Posting',
       component: Posting
+    },
+    {
+      path: '/Friend',
+      name: 'Friend',
+      component: Friend
+    },
+    {
+      path: '/comment',
+      name: 'comment',
+      component: comment
     }
 
     ]
