9640c6b8bbb7f8b88102d3fcd259826f5ad7ef09
dxhack <601239216@qq.com>
2017-09-15 19:35:06 +0800

    修改冲突文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-<<<<<<< HEAD
-import class1 from '@/components/class1'
-import Home from '@/components/Home'
 import login from '@/components/login'
-
-=======
 import Friend from '@/components/Friend'
 import Posting from '@/components/Posting'
->>>>>>> 3a35a4c93031fdb56c092e1fc5216c8a3c294645
+
 
 Vue.use(Router)
 export default new Router({
@@ -25,19 +20,14 @@ export default new Router({
       component: Friend
     },
     {
-<<<<<<< HEAD
-      path: '/Home',
-      name: 'This is Hi',
-      component: Home
-    },{
       path:'/login',
       name:'Tis is login',
       component:login
-=======
-      path: '/Posting',
-      name: 'Posting',
-      component: Posting
->>>>>>> 3a35a4c93031fdb56c092e1fc5216c8a3c294645
+    },
+      {
+        path: '/Posting',
+        name: 'Posting',
+        component: Posting
     }
 
     ]
