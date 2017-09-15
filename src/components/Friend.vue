85fa9de03e9d50a3bcd2e395d8bfca8a5bfe221d
sun fei <you@example.com>
2017-09-15 15:03:54 +0800

    xiu

---
+++ src/components/Friend.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="friend">
+  <mt-header title="我的好友">
+    <router-link to="/Home" slot="left">
+      <mt-button icon="back">我</mt-button>
+    </router-link>
+    <mt-button icon="more" slot="right"></mt-button>
+  </mt-header>
+  <ul>
+    <li v-for="todo in todos" >
+      {{ todo.text }}
+    </li>
+  </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'friend',
+  message:'hello',
+  data(){
+    return{
+           todos: [
+            { text: '学习 JavaScript,学习 Vue' },
+            { text: '创建激动人心的代码' }
+        ]
+    }
+   
+  },
+    methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .friend{
+        height:100%;
+        width:100%;
+    }
+    .aaa{
+        width:200px;
+        height:40px;
+    }
+    .mint-header{
+      color:black;
+      background:#eeeeee;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-import class1 from '@/components/class1'
 import Home from '@/components/Home'
+import Friend from '@/components/Friend'
 
 Vue.use(Router)
 export default new Router({
@@ -13,14 +13,14 @@ export default new Router({
       component: Hello
     },
     {
-      path: '/class1/:id',
-      name: 'This is Hi',
-      component: class1
-    },
-    {
       path: '/Home',
       name: 'This is Hi',
       component: Home
+    },
+    {
+      path: '/Friend',
+      name: 'This is Hi',
+      component: Friend
     }
 
   ]
